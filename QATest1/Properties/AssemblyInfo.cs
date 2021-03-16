1400e8da7bab6ebb5636b72f2977386ea048d2b5
johnnydm <johnnydm20@gmail.com>
2021-03-15 21:02:55 -0400

    Initial commit

---
+++ QATest1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("QATest1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("QATest1")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0a89402f-58a9-4e0a-b1e8-6621ddcba796")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ QATest1/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace QATest1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public static IWebDriver driver;
+
+        public static void setup(string website)
+        {
+            driver = new ChromeDriver(@"C:\Users\Johnny\source\repos\QATest1\QATest1\WebDrivers\chromedriver_win32");
+            driver.Navigate().GoToUrl(website);
+            driver.Manage().Window.Maximize();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            setup("http://automationpractice.com/index.php");
+
+            Actions builder = new Actions(driver);
+            IWebElement elemetBlouse = driver.FindElement(By.CssSelector("a[title='Blouse']"));
+            builder.MoveToElement(elemetBlouse).Build().Perform();
+
+            var addButton = driver.FindElement(By.CssSelector("a[data-id-product='2']"));
+            addButton.Click();
+        }
+
+        public void TestVerifyEmail()
+        {
+            setup("http://automationpractice.com/index.php");
+        }
+    }
+}
